e304fd1807689744404f262e7adea9f85ad57902
Camden Swita <camdenswita@gmail.com>
2017-06-21 23:38:19 -0700

    Add input validation against negative numbers and 0, provides user feedback in those cases.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,10 @@
 //back-end
 var conditional = function (userInput) {
+  if (isNaN(userInput)) {
+    alert("must enter a number!");
+  } else if (userInput <= 0) {
+    alert("Come on! Enter a number higher than 0!");
+  }
   for (var currentNumber = 1; currentNumber <= userInput; currentNumber += 1) {
     if (currentNumber % 3 === 0) {
       $("#list").append("<li>ping</li>");
@@ -9,9 +14,7 @@ var conditional = function (userInput) {
       $("#list").append("<li>ping pong</li>");
     } else if (currentNumber % 3 !== 0 || currentNumber % 5 !== 0 || currentNumber % 15 !== 0) {
       $("#list").append("<li>" + currentNumber + "</li>");
-    } else {
-      alert("Enter a number!")
-    };
+    }
   };
 };
 
@@ -19,6 +22,7 @@ var conditional = function (userInput) {
 //front-end
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
+
     $("#list").empty();
     var userInput = parseInt($("input#userNumber").val());
     conditional(userInput);
