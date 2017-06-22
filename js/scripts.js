9259fbdc41799415c5925fab536c0beb51bce5f0
Camden Swita <camdenswita@gmail.com>
2017-06-22 01:19:07 -0700

    Fix ssmall error in javascript to ensure numbers divisible by 15 show ping pong

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 //back-end
 var conditional = function (userInput) {
   for (var currentNumber = 1; currentNumber <= userInput; currentNumber += 1) {
-    if (currentNumber % 3 === 0) {
-      $("#list").append("<li>ping</li>");
+    if (currentNumber % 15 === 0) {
+      $("#list").append("<li>ping pong</li>");
     } else if (currentNumber % 5 === 0) {
       $("#list").append("<li>pong</li>");
-    } else if (currentNumber % 15 === 0) {
-      $("#list").append("<li>ping pong</li>");
+    } else if (currentNumber % 3 === 0) {
+      $("#list").append("<li>ping</li>");
     } else if (currentNumber % 3 !== 0 || currentNumber % 5 !== 0 || currentNumber % 15 !== 0) {
       $("#list").append("<li>" + currentNumber + "</li>");
     }
