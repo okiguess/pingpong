802f5f15966d236b80dba17f53297824498b55af
Camden Swita <camdenswita@gmail.com>
2017-06-21 23:16:54 -0700

    Finish back and and front end logic, fine tune ID names in DOM.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,25 @@
 //back-end
-var conditional = function (number) {
-  for (var currentNumber = 1; currentNumber <= number; currentNumber += 1) {
-    
-  }
-}
+var conditional = function (userInput) {
+  for (var currentNumber = 1; currentNumber <= userInput; currentNumber += 1) {
+    if (currentNumber % 3 === 0) {
+      $("#list").append("<li>ping</li>");
+    } else if (currentNumber % 5 === 0) {
+      $("#list").append("<li>pong</li>");
+    } else if (currentNumber % 15 === 0) {
+      $("#list").append("<li>ping pong</li>");
+    } else if (currentNumber % 3 !== 0 || currentNumber % 5 !== 0 || currentNumber % 15 !== 0) {
+      $("#list").append("<li>" + currentNumber + "</li>");
+    } else {
+      alert("Enter a number!")
+    };
+  };
+};
 
 
 //front-end
 $(document).ready(function() {
   $("#blanks form").submit(function(event) {
+    $("#list").empty();
     var userInput = parseInt($("input#userNumber").val());
     conditional(userInput);
     event.preventDefault();
